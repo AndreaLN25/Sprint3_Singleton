c4e2f27be46ebc549c8c94522a6e9363fff50dba
AndreaLN25 <andrealn14@gmail.com>
2023-12-04 17:20:43 +0100

    Ejercicio Singleton completo

---
+++ indexTigger.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once "tigger.php";
+
+$tigger1 = Tigger::getInstance();
+
+$tigger1->roar();
+$tigger1->roar();
+
+$tigger2 = Tigger::getInstance();
+$tigger2->roar();
+
+
+
+?>
+
--- tigger.php
+++ tigger.php
@@ -2,8 +2,17 @@
 
 class Tigger {
 
+    private static $instancia;
+
     private function __construct() {
             echo "Building character..." . PHP_EOL;
+    }
+    public static function getInstance() {
+        if (self::$instancia == null){
+            self::$instancia = new Tigger();
+        }
+        return self::$instancia;
+    
     }
 
     public function roar() {
